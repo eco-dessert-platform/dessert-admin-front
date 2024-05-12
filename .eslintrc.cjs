c33a61367aa7146a5d6856f545d02874b0d2f789
jijiseong <jiseong5036@gmail.com>
2024-05-12 16:43:12 +0900

    📦️ Chore :  lint 설정 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'prettier',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/comma-dangle': 'off'
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname
+  },
+  ignorePatterns: ['*.config.*', 'node_modules', '*.d.ts', '.eslintrc.cjs']
+};
