a5b18033bafacdf6f2313009d8c206f9a2d16952
jijiseong <jiseong5036@gmail.com>
2024-05-12 16:32:38 +0900

    📦️ Chore :  환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <Button type="button" variant="contained">
+      Hello MUI
+    </Button>
+  );
+}
+
+export default App;
