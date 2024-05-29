90ad8d7c33f605b88d6aa1e35501a92082fb17eb
Taeyoon Kwon <47032283+TaeyoonKwon@users.noreply.github.com>
2024-05-29 16:00:41 +0900

    📦️ Chore : match linker with dessert-front and create basic components

---
+++ app/global/GlobalHeader.tsx
@@ -0,0 +1,11 @@
+export default function GlobalHeader() {
+  return (
+    <>
+      <header className="sticky top-0 flex h-[80px] w-full items-center bg-foreground px-5">
+        <section>
+          <p className="text-lg text-background">BBANGLE ADMIN</p>
+        </section>
+      </header>
+    </>
+  );
+}
